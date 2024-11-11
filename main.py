153ff307680d2e3b0cad83be7387e33a9bf1dfc6
Mehmetmrtgl <mehmetmrt1999@gmail.com>
2024-11-11 20:29:03 +0300

    Add function to extract person data from the webpage
    Refactored the web scraping code to include a new function  that extracts the names of people from the list on the webpage.

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ url = "https://bodysize.org/en"
 url_request = requests.get(url)
 soup = BeautifulSoup(url_request.text, 'html.parser')
 
-print(soup)
+def get_person_data(soup):
+    persons_ul = soup.find('ul', class_='persons')
+    if persons_ul:
+        persons_ul_li = persons_ul.find_all('li')
+        list_items = []
+
+        for person in persons_ul_li:
+            name_link = person.find('a', class_='name')
+            if name_link:
+                list_items.append(name_link)
+
+        return list_items
+    return []
+
+first_page_items = get_person_data(soup)
+print(first_page_items)
